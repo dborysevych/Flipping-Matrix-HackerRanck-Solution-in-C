bcd74194cd86f4d9875d9155b13bc08e2b56d780
Denys <102409171+dborysevych@users.noreply.github.com>
2022-04-18 20:37:47 +0200

    Update flippingMatrix.c

--- flippingMatrix.c
+++ flippingMatrix.c
@@ -2,48 +2,8 @@
 	Denys Borysevych
 	HackerRanck Solution in C
 	Flipping Matrix
+	18.03.2022
 					*/
-
-
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void printMatrix(int** matrix, int matrix_rows, int matrix_columns)
-{
-    printf("\n---------------------\n");
-    for(int i = 0; i < matrix_rows;i++)
-    {
-        for(int j = 0; j < matrix_columns;j++)
-        {
-            printf("%4d", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n---------------------\n");
-
-}
-
-int maxm(int a,int b,int c,int d){
-    int max;
-    if(a>=b && a>=c && a>=d){
-        max=a;
-    }
-    else if(a<=b && b>=c && b>=d)
-        max=b;
-    else if(c>=b && a<=c && c>=d)
-        max=c;
-   else
-        max=d;
-   return max;
-}
 
 int flippingMatrix(int matrix_rows, int matrix_columns, int** x) {
 
@@ -60,45 +20,3 @@ int flippingMatrix(int matrix_rows, int matrix_columns, int** x) {
 }
 
 
-
-
-int main()
-{
-    int n = 2;
-
-        int** matrix = malloc((2 * n) * sizeof(int*));
-
-        for (int i = 0; i < 2 * n; i++) 
-            *(matrix + i) = malloc((2 * n) * (sizeof(int)));
-
-           matrix[0][0] = 112;
-           matrix[0][1] = 42;
-           matrix[0][2] = 83;
-           matrix[0][3] = 119;
-           
-           matrix[1][0] = 56;
-           matrix[1][1] = 125;
-           matrix[1][2] = 56;
-           matrix[1][3] = 49;
-           
-           matrix[2][0] = 15;
-           matrix[2][1] = 78;
-           matrix[2][2] = 101;
-           matrix[2][3] = 43;
-           
-           matrix[3][0] = 62;
-           matrix[3][1] = 98;
-           matrix[3][2] = 114;
-           matrix[3][3] = 108;
-        
-        int result = flippingMatrix(2 * n, 2 * n, matrix);
-
-        fprintf(stdout, "%d\n", result);
-
-        if(result == 414)
-            printf("OK");
-        else
-            printf("NO OK");
-
-    return 0;
-}
